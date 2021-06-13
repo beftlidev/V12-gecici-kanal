f15ace5a446e064829e54dbf27aa55771215b9a1
iUgur <77486662+iUgur@users.noreply.github.com>
2021-06-13 10:03:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-Yapılıyor...!
+const TempChannels = require("discord-temp-channels");
+const tempChannels = new TempChannels(client);
+
+client.on("ready", () => {
+    if (!db.get("temp-channels")) db.set("temp-channels", []);
+    db.get("temp-channels").forEach((channelData) => {
+        tempChannels.registerChannel(channelData.channelID, channelData.options);
+    });
+});
